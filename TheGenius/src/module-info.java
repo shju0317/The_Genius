f344a7378b733ee0b226614a23e5329354ebb862
OJH <dhjungk@naver.com>
2024-04-16 13:35:31 +0900

    로그인회원가입

--- TheGenius/src/module-info.java
+++ TheGenius/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module TheGenius {
+	requires java.sql;
 }
--- TheGenius/src/view/Main.java
+++ TheGenius/src/view/Main.java
@@ -2,11 +2,15 @@ package view;
 
 import java.util.Scanner;
 
+
+
+
+
 public class Main {
 	public static void main(String[] args) {
 		//test
 		Scanner sc = new Scanner(System.in);
-		
+		controller.MemberCon con = new controller.MemberCon();
 		System.out.println("\r\n"
 				+ "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n"
 				+ "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n"
@@ -51,10 +55,15 @@ public class Main {
 			
 			if(select == 1) { // 로그인
 				System.out.println("===== 로그인 =====");
-				
-				
+				System.out.println("id");
+				String ID = sc.next();
+				System.out.println("pw");
+				String PW = sc.next();
+				model.MemberDTO dto = new model.MemberDTO(ID,PW);
+				con.login(dto);
 			}else if(select == 2){ // 회원가입 
 				System.out.println("===== 회원가입 =====");
+				con.joinmember();
 								
 			}else{
 				System.out.println("로그인이 필요합니다 :)");
