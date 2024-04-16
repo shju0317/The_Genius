9741c6077e828f9b4db5fbf062e26f2ed96fbde1
KES <keskes220@gmail.com>
2024-04-16 17:34:18 +0900

    사칙연산게임완료

---
+++ src/Controller/SameNumController.java
@@ -0,0 +1,26 @@
+package Controller;
+
+import Model.SameNumDAO;
+
+public class SameNumController {
+
+    public int calculate(int num1, String operator, int num2) {
+        int result = 0;
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+        }
+        return result;
+    }
+
+}
---
+++ src/Model/SameNumDTO.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class SameNumDTO {
+
+	private int num;
+	private String str;
+
+	public SameNumDTO(int num) {
+		this.num=num;
+	}
+	
+	public SameNumDTO(String str) {
+		this.str=str;
+	}
+	
+	
+
+}
